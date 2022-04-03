d639c0998fceb9d2503ffa2cc66447c397268399
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-04-02 23:52:41 -0700

    Skeleton structure

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get 6 Images From NASA API
+// async function getNasaPictures() {
+//   try {
+//     const res = await axios.get(apiUrl);
+//     const data = await res.data;
+//     resultsArray = data;
+//   } catch (error) {
+//     console.log('Whoops', error);
+//   }
+// }

26d42cb43dba59ccb8495b081ac3f81d4d22f3e2
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-04-04 12:52:17 -0700

    Assign firstVal &  store operator

--- script.js
+++ script.js
@@ -2,14 +2,37 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  calculatorDisplay.textContent === "0" ? calculatorDisplay.textContent = number : calculatorDisplay.textContent += number;
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+  } else {
+    calculatorDisplay.textContent === "0" ? calculatorDisplay.textContent = number : calculatorDisplay.textContent += number;
+  }
 }
 
 function addDecimal() {
+  if(awaitingNextValue) return;
   if (!calculatorDisplay.textContent.includes('.')) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
   }
+}
+
+function useOperator(operator) {
+  const currentValue = calculatorDisplay.textContent;
+  // Assign firstVal if no value
+  if (!firstValue) {
+    firstValue = currentValue
+  } else {
+    console.log('currentValue: ', currentValue);
+  }
+  // Ready for next value, store operator
+  awaitingNextValue = true
+  operatorValue = operator;
+  console.log(`firstValue: ${firstValue} operatorValue: ${operatorValue}`);
 }
 
 // add event Listeners for numbers, operators, decimal buttons
@@ -17,7 +40,7 @@ inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('operator')) {
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
     inputBtn.addEventListener('click', () => addDecimal());
   }
@@ -25,6 +48,9 @@ inputBtns.forEach((inputBtn) => {
 
 // Reset Display
 function resetAll() {
+  firstValue = 0;
+  operatorValue = '';
+  awaitingNextValue = false;
   calculatorDisplay.textContent = '0';
 }
 
